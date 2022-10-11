8f82cf4aa3c3cfd435037e95a8d64e44184f05b0
Ismael4474 <iescude7@hotmail.com>
2022-10-12 00:31:20 +0200

    Initial commit

---
+++ app/src/main/java/com/ismaellopez/eschoolmanager_mobil/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ismaellopez.eschoolmanager_mobil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText inputUser, inputPassword;
+    private Button btoInici;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inputUser = findViewById(R.id.inputUsuari);
+        inputPassword = findViewById(R.id.inputPassword);
+        btoInici = findViewById(R.id.btoIniciarSessio);
+    }
+    //metodo para el boton
+    public void iniciSessio(View view){
+        Toast.makeText(this, "Conectan!!!!!", Toast.LENGTH_SHORT).show();
+    }
+
+}
